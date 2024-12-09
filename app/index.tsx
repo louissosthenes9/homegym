9a821bf6ddc6bf5a5203df02e24a93ffc01bfef5
louissosthenes9 <louissosthenes9@gmail.com>
2024-12-09 18:51:13 +0300

    added new packages

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const index = () => {
+  return (
+    <View className='flex-1 justify-center items-center'>
+        <Text>
+            our first screen
+        </Text>
+    </View>
+  )
+}
+
+export default index
